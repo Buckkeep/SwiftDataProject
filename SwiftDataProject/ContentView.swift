f90425c4f052950f7ebbabe25a4b02642dcc1f96
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-08-14 15:58:13 +0100

    Dynamically sorting and filtering @Query with SwiftUI

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -10,18 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(filter: #Predicate<User> { user in
-        user.name.localizedStandardContains("R") &&
-        user.city == "London"
-    }, sort: \User.name) var users: [User]
+    @State private var showingUpComingOnly = false
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate)
+    ]
     
     var body: some View {
         NavigationStack {
-            List(users) { user in
-                NavigationLink(value: user) {
-                    Text(user.name)
-                }
-            }
+            UsersView(minimumJoinDate: showingUpComingOnly ? .now : .distantPast, sortOrder: sortOrder)
             .navigationTitle("Users")
             .toolbar {
                 Button("Add Sample", systemImage: "plus") {
@@ -31,12 +28,30 @@ struct ContentView: View {
                     let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
                     let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
                     let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
-
+                    
                     modelContext.insert(first)
                     modelContext.insert(second)
                     modelContext.insert(third)
                     modelContext.insert(fourth)
-
+                    
+                }
+                Button(showingUpComingOnly ? "Show Everyone" : "Show Upcoming") {
+                    showingUpComingOnly.toggle()
+                }
+                
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by Name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                SortDescriptor(\User.joinDate)
+                            ])
+                        Text("Sort by Join Date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name)
+                            ])
+                    }
                 }
             }
         }
