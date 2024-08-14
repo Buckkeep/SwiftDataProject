4d4afcdc2debccdd9b1a178217f17cc9316b8617
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-08-14 16:26:06 +0100

    Relationships with SwiftData, SwiftUI, and @Query

---
+++ SwiftDataProject/UsersView.swift
@@ -0,0 +1,55 @@
+//
+//  UsersView.swift
+//  SwiftDataProject
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 14/08/2024.
+//
+
+import SwiftData
+import SwiftUI
+
+struct UsersView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var users: [User]
+    
+    var body: some View {
+        List(users) { user in
+            HStack {
+                Text(user.name)
+                
+                Spacer()
+                
+                Text(String(user.jobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
+        }
+        .onAppear(perform: addSample)
+    }
+    
+    init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
+        _users = Query(filter: #Predicate<User> { user in
+            user.joinDate >= minimumJoinDate
+        }, sort: sortOrder)
+    }
+    
+    func addSample() {
+        let user1 = User(name: "Marie Kondo", city: "Tokyo", joinDate: .now)
+        let job1 = Job(name: "Organise Sock Drawer", priority: 1)
+        let job2 = Job(name: "Spark Joy", priority: 2)
+        
+        modelContext.insert(user1)
+        
+        user1.jobs.append(job1)
+        user1.jobs.append(job2)
+    }
+}
+
+#Preview {
+    UsersView(minimumJoinDate: .now, sortOrder: [SortDescriptor(\User.name)])
+        .modelContainer(for: User.self)
+}
